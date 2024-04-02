f80136935cfb7b67e5eb03b48dcb606ee89e4178
KairenVk <kamilsurdacki01@gmail.com>
2024-04-02 13:06:18 +0200

    Revert "Implement table & API"
    This reverts commit ce7cbc4c80edaef73f2a89a57446e698e8e17702.

--- src/ItemObject.ts
+++
@@ -1,7 +0,0 @@
-export interface ItemObject {
-    count: number,
-    has_synonyms: boolean,
-    is_moderator_only: boolean,
-    is_required: boolean,
-    name: string
-}
--- src/ResponseObject.ts
+++
@@ -1,8 +0,0 @@
-import {ItemObject} from "./ItemObject";
-
-export interface ResponseObject {
-    has_more: boolean,
-    items: Array<ItemObject>,
-    quota_max: number,
-    quota_remaining: number
-}
