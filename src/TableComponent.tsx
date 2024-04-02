ce7cbc4c80edaef73f2a89a57446e698e8e17702
KairenVk <kamilsurdacki01@gmail.com>
2024-04-02 13:01:43 +0200

    Implement table & API

--- src/TableComponent.tsx
+++ src/TableComponent.tsx
@@ -1,15 +1,236 @@
-import {Paper, TableContainer, TableHead, TableRow} from "@mui/material";
+import {
+    Box,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow, TableSortLabel
+} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {ResponseObject} from "./ResponseObject";
+import { visuallyHidden } from '@mui/utils';
+import {ItemObject} from "./ItemObject";
 
-function Table() {
-    return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        
-                    </TableRow>
-                </TableHead>
-            </Table>
-        </TableContainer>
-    )
+
+
+type Order = 'asc' | 'desc';
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
 }
+
+function getComparator<Key extends keyof any>(
+    order: Order,
+    orderBy: Key,
+): (
+    a: { [key in Key]: number | string | boolean},
+    b: { [key in Key]: number | string | boolean},
+) => number {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+interface HeadCell {
+    disablePadding: boolean;
+    id: keyof ItemObject;
+    label: string;
+    numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+    {
+        id: 'name',
+        numeric: false,
+        disablePadding: false,
+        label: 'Tag',
+    },
+    {
+        id: 'count',
+        numeric: true,
+        disablePadding: false,
+        label: 'Post Count',
+    }
+];
+
+interface EnhancedTableProps {
+    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof ItemObject) => void;
+    order: Order;
+    orderBy: string;
+    rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+    const {order, orderBy, onRequestSort } =
+        props;
+    const createSortHandler =
+        (property: keyof ItemObject) => (event: React.MouseEvent<unknown>) => {
+            onRequestSort(event, property);
+        };
+
+    return (
+        <TableHead>
+            <TableRow>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'normal'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+function TableComponent() {
+    let emptyObj: ResponseObject = {has_more:false,items:[],quota_max:0,quota_remaining:0}
+    const [order, setOrder] = React.useState<Order>('asc');
+    const [orderBy, setOrderBy] = React.useState<keyof ItemObject>('name');
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [tagObject, setTagObject] = useState<ResponseObject>(emptyObj);
+    const [loadingState, setLoadingState] = useState("loaded");
+
+
+    useEffect(() => {
+        fetch("https://api.stackexchange.com/tags?site=stackoverflow&pagesize=100", {
+            method: "GET",
+        })
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log(responseData)
+                setTagObject(responseData);
+                setLoadingState("loaded");
+
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoadingState("error");
+            })
+    }, []);
+
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+
+    };
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const handleRequestSort = (
+        event: React.MouseEvent<unknown>,
+        property: keyof ItemObject,
+    ) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - tagObject.items.length) : 0;
+
+    const visibleRows = React.useMemo(
+        () =>
+            tagObject.items.sort(getComparator(order, orderBy)).slice(page * rowsPerPage,
+                page * rowsPerPage + rowsPerPage),
+        [order, orderBy, page, rowsPerPage, tagObject],
+    );
+
+    if (loadingState === "loading") {
+        return (
+            <h1>LOADING</h1>
+        )
+    }
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Paper sx={{ width: '100%', mb: 2 }}>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25, 50]}
+                    component="div"
+                    count={tagObject.items.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                    align='center'
+                />
+                <TableContainer>
+                    <Table
+                        sx={{ minWidth: 750 }}
+                        aria-labelledby="tableTitle"
+                        size={'medium'}
+                    >
+                        <EnhancedTableHead
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                            rowCount={tagObject.items.length}
+                        />
+                        <TableBody>
+                            {visibleRows.map((row, index) => {
+                                const labelId = `enhanced-table-checkbox-${index}`;
+
+                                return (
+                                    <TableRow
+                                        hover
+                                        role="checkbox"
+                                        tabIndex={-1}
+                                        key={index}
+                                        sx={{ cursor: 'pointer' }}
+                                    >
+                                        <TableCell
+                                            component="th"
+                                            id={labelId}
+                                            scope="row"
+                                            padding="none"
+                                        >
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell align="right">{row.count}</TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                            {emptyRows > 0 && (
+                                <TableRow
+                                    style={{
+                                        height: (53) * emptyRows,
+                                    }}
+                                >
+                                    <TableCell colSpan={6} />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </Box>
+    );
+}
+
+export default TableComponent;
